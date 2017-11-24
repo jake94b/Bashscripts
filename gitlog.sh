3cb5d87fd31e90fa11140741d844aaaedd7dd95f
jake94b <jbutler1@csustan.edu>
2017-11-24 15:19:19 -0800

    Final Update

--- gitlog.sh
+++ gitlog.sh
@@ -16,25 +16,25 @@ read MSG
 
 if [ -e log.txt ]; then
 
-echo ----------`date`--------- 2>> log.txt
+echo ----------`date`--------- 2>>log.txt
 
-git commit -m "$MSG" 2>> log.txt
+git commit -m "$MSG" 2>>log.txt
 
-git push origin master 2>> log.txt
+git push origin master 2>>log.txt
 
 echo ----------------------------------------------- 2>>log.txt
 
 else
 
-echo Beginning of log file 2> log.txt
+echo Beginning of log file 2>log.txt
 
-echo ----------`date`---------- 2>> log.txt
+git commit -m "$MSG" 2>>log.txt
 
-git commit -m "$MSG" 2>> log.txt
+echo `date` 2>>log.txt
 
-git push origin master 2>> log.txt
+git push origin master 2>>log.txt
 
-echo ----------------------------------------------- 2>> log.txt
+echo ----------------------------------------------- 2>>log.txt
 
 
 fi
