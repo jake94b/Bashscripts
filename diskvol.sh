668527961c2e2551d7efb7c90247b6a3d8065bb4
jake94b <jbutler1@csustan.edu>
2017-11-28 14:23:18 -0800

    Root check and Volume reading

--- diskvol.sh
+++ diskvol.sh
@@ -1,2 +1,22 @@
 #!/bin/bash
 ##Shows disk space usage by group
+
+
+#If a list of volume groups is provided, use those
+#Otherwise, list all volume group disk usage
+VGS=$@
+[ -z "$VGS" ] && {
+    #Tests if being run as root
+        [ "$EUID" -eq 0 ] || {
+            echo "Please run this as root"
+            exit 1
+}
+    #Retrieve all volume groups
+    $VGS=$(vgs --noheadings | awk '{print $1}' )
+
+
+   TMPFILE=$(mktemp --suffix -$(basename $0)) 
+    
+for VG in $VGS
+    do
+done
