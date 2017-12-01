109e1fda17c7e3a4da2312a3d1f97e856d142066
Jake Butler <jbutler1@csustan.edu>
2017-12-01 11:25:49 -0800

    Delete gitlog.sh

--- gitlog.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-##Script to keep log of all git commits for a repository
-
-git add .
-read -p "Commit Description: " desc 
-git commit -m "$desc"
-echo `date` 2>>log.txt
-git push origin master 2>> log.txt
