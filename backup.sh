c6d8c9d827a214634d5a683a7f8ddfdaa34371a0
jake94b <jbutler1@csustan.edu>
2017-12-01 11:30:11 -0800

    Done

--- backup.sh
+++ backup.sh
@@ -5,9 +5,12 @@ if [[ $EUID -ne 0 ]]; then
    echo "This script must be run as root" 
    exit 1
 fi
+
+TIME=`date +%b-%d-%y`
+FNAME=backup-$TIME.tar.gz
 echo Please specify the directory you would like to back up
 read $SRCNAME
 echo Please specify where you would like to direct backup to
 read $DESTNAME
-tar -cvpzf $DESTNAME.tar.gz $SRCNAME
+tar -cvpzf $DESTNAME/$FNAME $SRCNAME
 
