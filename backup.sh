c720105382481dc117032b5cff6f4d2449a33d86
jake94b <jbutler1@csustan.edu>
2017-12-01 11:46:37 -0800

    Added check for folder size

--- backup.sh
+++ backup.sh
@@ -6,10 +6,28 @@ if [[ $EUID -ne 0 ]]; then
    exit 1
 fi
 
+temp="/tmp/available.$$"
+
+trap "rm -f $temp" EXIT
+
+cat << 'EOF' >$temp
+    { sum += $4 }
+    
+END {mb = sum / 1024
+     gb = mb / 1024
+     printf "%.0f MB (%.2fGB) of available disk space\n" , mb, gb
+    }
+EOF
+
+df -k | awk -f $temp
+
+exit 0
+
 TIME=`date +%b-%d-%y`
 FNAME=backup-$TIME.tar.gz
 echo Please specify the directory you would like to back up
 read $SRCNAME
+SRCSIZE="$(du -sh)"
 echo Please specify where you would like to direct backup to
 read $DESTNAME
 tar -cvpzf $DESTNAME/$FNAME $SRCNAME
