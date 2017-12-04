03028b4122d28ded3461ca470d4acc718c120be5
jake94b <jbutler1@csustan.edu>
2017-12-04 12:32:40 -0800

    Attempt to fix

--- backup.sh
+++ backup.sh
@@ -6,28 +6,15 @@ if [[ $EUID -ne 0 ]]; then
    exit 1
 fi
 
-temp="/tmp/available.$$"
 
-trap "rm -f $temp" EXIT
-
-cat << 'EOF' >$temp
-    { sum += $4 }
-    
-END {mb = sum / 1024
-     gb = mb / 1024
-     printf "%.0f MB (%.2fGB) of available disk space\n" , mb, gb
-    }
-EOF
-
-df -k | awk -f $temp
-
-echo $gb
 TIME=`date +%b-%d-%y`
 FNAME=backup-$TIME.tar.gz
 echo Please specify the directory you would like to back up
 read $SRCNAME
+$FREESPCE="$(df -h | tail -1 | awk '{print $4)')"
+echo $FREESPACE
 SRCSIZE="$(du -sh $SRCNAME | cut -f 1)"
-if 
+echo $SRCSIZE
 echo Please specify where you would like to direct backup to
 read $DESTNAME
 tar -cvpzf $DESTNAME/$FNAME $SRCNAME
