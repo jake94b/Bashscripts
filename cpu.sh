e796d703822432987f749708295bc8d2ce52e101
jake94b <jbutler1@csustan.edu>
2017-12-05 12:29:40 -0800

    NOW done

--- cpu.sh
+++ cpu.sh
@@ -1,34 +1,31 @@
 #!/bin/bash
-#Displays current CPU utilization
+##Displays CPu usage at fixed intervals
 
-PREV_IDLE=0
+
 PREV_TOTAL=0
-
+PREV_IDLE=0
+ 
 while true; do
-
-	#Get CPU Statistics
-	CPU=(`sed -n 's/^cpu\s//p' /proc/stat`)
-	IDLE=${CPU[3]} #Just idle time 
-
-	
-	#Calculate total CPU time
-	TOTAL=0
-	for VALUE in "${CPU[@]}"; do
-		let "TOTAL=$TOTAL+$VALUE"
-	don
-
-
-
-#Calculate CPU Usage from last time
-	let "DIFFIDLE=$IDLE-$PREV_IDLE"
-  	let "DIFFTOTAL=$TOTAL-$PREV_TOTAL"
-  	let "DIFFUSAGE=(1000*($DIFF_TOTAL-$DIFF_IDLE)/$DIFF_TOTAL+5)/10"
- echo -en "\rCPU: $DIFFUSAGE%  \b\b"
-
-#Store CPU Usage for next cycle
-PREV_IDLE="$IDLE"
-PREV_TOTAL="$TOTAL"
-
-#Wait
-	sleep 1
-done 
+  # Get the total CPU statistics
+  CPU=(`sed -n 's/^cpu\s//p' /proc/stat`)
+  IDLE=${CPU[3]}
+ 
+  # Calculate the total CPU time.
+  TOTAL=0
+  for VALUE in "${CPU[@]}"; do
+    let "TOTAL=$TOTAL+$VALUE"
+  done
+ 
+  # Calculate the CPU usage since we last checked.
+  let "DIFF_IDLE=$IDLE-$PREV_IDLE"
+  let "DIFF_TOTAL=$TOTAL-$PREV_TOTAL"
+  let "DIFF_USAGE=(1000*($DIFF_TOTAL-$DIFF_IDLE)/$DIFF_TOTAL+5)/10"
+  echo -en "\rCPU: $DIFF_USAGE%  \b\b"
+ 
+  # Remember the total and idle CPU times for the next check.
+  PREV_TOTAL="$TOTAL"
+  PREV_IDLE="$IDLE"
+ 
+  # Wait before checking again.
+  sleep 1
+done
