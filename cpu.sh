9a023af574b99bd525c7c65379d0e4f7c6412649
jake94b <jbutler1@csustan.edu>
2017-12-05 11:26:12 -0800

    Calculate CPU usage

--- cpu.sh
+++ cpu.sh
@@ -13,14 +13,20 @@ while true; do
 	
 	#Calculate total CPU time
 	TOTAL=0
+	for VALUE in "${CPU[@]}"; do
+		let "TOTAL=$TOTAL+$VALUE"
+	don
 
 
 
 #Calculate CPU Usage from last time
+	let "DIFFIDLE=$IDLE-$PREV_IDLE"
+	let "DIFTOT=$TOTAL-$PREV_TOTAL"
 
 
 #Store CPU Usage for next cycle
-
+PREV_IDLE="$IDLE"
+PREV_TOTAL="$TOTAL"
 
 #Wait
 	sleep 1
