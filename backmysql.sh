01b62866cfc8a50519db20845d69220ce4221a9b
jake94b <jbutler1@csustan.edu>
2017-12-08 18:54:56 -0800

    Added datetime and checks

--- backmysql.sh
+++ backmysql.sh
@@ -17,4 +17,25 @@ BACKDIR="/home/backup/mysql"
 DB_SERV="127.0.0.1"
 SERV_NAME="$(hostname)"
 
+USR="root"
+
+##Will need to input password
+PASSWD=" "
+
+YR="$(date+"%Y")"
+MNTH="$(date+"%M")"
+DY="$(date+"%D")"
+
+BACKDIR="$BACKDIR/$YR"
+
+
+#Makes the Backup directory if it does not exist
+if [! -d $BACKDIR ]; then
+	/bin/mkdir $BACKDIR
+fi
+
+if [! -w $BACKDIR }; then
+	#Todo: Make sure directory is accessible
+fi
+
 
